1bc198b3f79b28c1ff37c8fab683ef8d46970d99
mmmtl <maria.taili@georgebrown.ca>
2026-02-13 17:59:15 -0500

    complete w05

--- Week05/Lab05.py
+++ Week05/Lab05.py
@@ -80,27 +80,22 @@ def fizz_buzz(n):
     result = []
 
     # Loop from 1 to n (inclusive)
-    for i in range(1, n + 1):
-        if i % 3 == 0 and i % 5 == 0:
-            result.append("FizzBuzz")
-        elif i % 3 == 0:
-            result.append("Fizz")
-        elif i % 5 == 0:
-            result.append("Buzz")
-        else:
-            result.append(str(i))
-    return result
     # Hint: Use range(1, n + 1)
-
+    for i in range(1, n + 1):
         # Check if divisible by BOTH 3 and 5 FIRST
         # Hint: if i % 3 == 0 and i % 5 == 0
-
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("FizzBuzz")
         # Then check if divisible by 3 only
-
+        elif i % 3 == 0:
+            result.append("Fizz")
         # Then check if divisible by 5 only
-
+        elif i % 5 == 0:
+            result.append("Buzz")
         # Otherwise, append the number as a string
         # Hint: result.append(str(i))
+        else:
+            result.append(str(i))
 
     return result
 
